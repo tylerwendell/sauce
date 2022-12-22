9e68b1071eb979f2c256749fb755d7ab4943cc10
Tyler Wendell <twendell@centripetalnetworks.com>
2022-12-22 17:15:06 -0500

    Adding more traits

--- src/lisp-parser/src/operators.rs
+++ src/lisp-parser/src/operators.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::error::Error;
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum Primitive {
@@ -8,6 +10,27 @@ pub enum Primitive {
 }
 
 use std::ops;
+
+#[derive(Debug)]
+pub struct OperationError {
+    message: String,
+}
+
+
+impl Error for OperationError {}
+
+impl fmt::Display for OperationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,"{}",self.message)
+    }
+}
+
+impl From<&str> for OperationError {
+    fn from(message: &str) -> Self {
+        OperationError { message: message.to_string() }
+    }
+}
+
 impl ops::Add for Primitive {
 	type Output = Primitive;
 
@@ -87,17 +110,59 @@ impl ops::Sub for Primitive {
 impl ops::Mul for Primitive {
 	type Output = Primitive;
 
-    fn add(self, other: Primitive) -> Primitive {
+    fn mul(self, other: Primitive) -> Primitive {
         match (self, other) {
-            (Primitive::Integer(x), Primitive::Integer(y)) => Primitive::Integer(x + y),
-            (Primitive::DoublePrecisionFloat(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x + y),
-            (Primitive::Str(x), Primitive::Str(y)) => Primitive::Str(x + &y),
-            (Primitive::Integer(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x as f64 + y),
-            (Primitive::DoublePrecisionFloat(x), Primitive::Integer(y)) => Primitive::DoublePrecisionFloat(x + y as f64),
-            (Primitive::Integer(x), Primitive::Str(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::Str(x), Primitive::Integer(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(format!("{}{}", x, y)),
+            (Primitive::Integer(x), Primitive::Integer(y)) => Primitive::Integer(x * y),
+            (Primitive::DoublePrecisionFloat(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x * y),
+            (Primitive::Str(a), Primitive::Str(b)) => {
+                let mut result = String::new();
+                for (x, y) in a.chars().zip(b.chars()) {
+                    result.push(x);
+                    result.push(y);
+                }
+                if a.len() > b.len() {
+                    result.push_str(&a[b.len()..]);
+                }
+                if b.len() > a.len() {
+                    result.push_str(&a[b.len()..]);
+                }
+                
+                Primitive::Str(result)
+            },
+            (Primitive::Vector(a), Primitive::Vector(b)) => {
+                let mut result = Vec::new();
+                let mut longer = a.clone();
+                let mut shorter = b.clone();
+                if a.len() < b.len() {
+                    longer = b.clone();
+                    shorter = a.clone();
+                }
+                for (x, y) in shorter.iter().zip(longer.iter()) {
+                    result.push(*x * *y);
+                }
+                result.extend(longer[shorter.len()..].iter().cloned());
+                Primitive::Vector(result)
+            },
+            (Primitive::Integer(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x as f64 * y),
+            (Primitive::DoublePrecisionFloat(x), Primitive::Integer(y)) => Primitive::DoublePrecisionFloat(x * y as f64),
+            (Primitive::Integer(x), Primitive::Str(y)) => Primitive::Str(y.repeat(x as usize)),
+            (Primitive::Str(x), Primitive::Integer(y)) => Primitive::Str(x.replace(&y.to_string(),"")),
+            (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => {
+                let i = x.ceil() as i32;
+                let result = y.repeat(i as usize);
+                let fraction = (x/x.ceil()) * result.len() as f64;
+                let final_pos = fraction.floor() as i32;
+                let final_u = final_pos as usize;
+                Primitive::Str(result[0..final_u].to_string())
+            },
+            // Start below here
+            (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(x.replace(&y.to_string(),"")),
+            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
+            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
+            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
+            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
     }
     }
 }
