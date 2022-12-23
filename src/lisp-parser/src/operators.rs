18792e8073abf4dfb53fe34cde7bf06290a641e7
Tyler Wendell <tyler.wendell09@gmail.com>
2022-12-23 00:20:14 -0500

    I think I am done, for now, with the basic operations: (+ - * /). The string operations are not entirely rational. We need to think through what these operations can mean and make sure the reciprocol makes sense.

--- src/lisp-parser/src/operators.rs
+++ src/lisp-parser/src/operators.rs
@@ -48,7 +48,7 @@ impl ops::Add for Primitive {
                     shorter = a.clone();
                 }
                 for (x, y) in shorter.iter().zip(longer.iter()) {
-                    result.push(*x + *y);
+                    result.push(x.clone() + y.clone());
                 }
                 result.extend(longer[shorter.len()..].iter().cloned());
                 Primitive::Vector(result)
@@ -59,12 +59,12 @@ impl ops::Add for Primitive {
             (Primitive::Str(x), Primitive::Integer(y)) => Primitive::Str(format!("{}{}", x, y)),
             (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => Primitive::Str(format!("{}{}", x, y)),
             (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::Integer(value)).collect()),
-            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::Str(value)).collect()),
-            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::Integer(value)).collect()),
-            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::Str(value)).collect()),
+            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::Integer(value)).collect()),
+            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::Str(value.clone())).collect()),
+            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::Integer(value)).collect()),
+            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| x.clone() + Primitive::Str(value.clone())).collect()),
     }
     }
 }
@@ -86,7 +86,7 @@ impl ops::Sub for Primitive {
                     shorter = a.clone();
                 }
                 for (x, y) in shorter.iter().zip(longer.iter()) {
-                    result.push(*x - *y);
+                    result.push(x.clone() - y.clone());
                 }
                 result.extend(longer[shorter.len()..].iter().cloned());
                 Primitive::Vector(result)
@@ -97,12 +97,12 @@ impl ops::Sub for Primitive {
             (Primitive::Str(x), Primitive::Integer(y)) => Primitive::Str(x.replace(&y.to_string(),"")),
             (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => Primitive::Str((x.to_string()).replace(&y,"")),
             (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(x.replace(&y.to_string(),"")),
-            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
-            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
-            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
-            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
+            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::Integer(value)).collect()),
+            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::Str(value.clone())).collect()),
+            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::Integer(value)).collect()),
+            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| x.clone() - Primitive::Str(value.clone())).collect()),
     }
     }
 }
@@ -138,7 +138,7 @@ impl ops::Mul for Primitive {
                     shorter = a.clone();
                 }
                 for (x, y) in shorter.iter().zip(longer.iter()) {
-                    result.push(*x * *y);
+                    result.push(x.clone() * y.clone());
                 }
                 result.extend(longer[shorter.len()..].iter().cloned());
                 Primitive::Vector(result)
@@ -155,14 +155,20 @@ impl ops::Mul for Primitive {
                 let final_u = final_pos as usize;
                 Primitive::Str(result[0..final_u].to_string())
             },
-            // Start below here
-            (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(x.replace(&y.to_string(),"")),
-            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
-            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x + Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
-            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Integer(value)).collect()),
-            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::DoublePrecisionFloat(value)).collect()),
-            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| *x - Primitive::Str(value)).collect()),
+            (Primitive::Str(y), Primitive::DoublePrecisionFloat(x)) => {
+                let i = x.ceil() as i32;
+                let result = y.repeat(i as usize);
+                let fraction = (x/x.ceil()) * result.len() as f64;
+                let final_pos = fraction.floor() as i32;
+                let final_u = final_pos as usize;
+                Primitive::Str(result[0..final_u].to_string())
+            },
+            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::Integer(value)).collect()),
+            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::Str(value.clone())).collect()),
+            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::Integer(value)).collect()),
+            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| x.clone() * Primitive::Str(value.clone())).collect()),
     }
     }
 }
@@ -170,17 +176,84 @@ impl ops::Mul for Primitive {
 impl ops::Div for Primitive {
 	type Output = Primitive;
 
-    fn add(self, other: Primitive) -> Primitive {
+    fn div(self, other: Primitive) -> Primitive {
         match (self, other) {
-            (Primitive::Integer(x), Primitive::Integer(y)) => Primitive::Integer(x + y),
-            (Primitive::DoublePrecisionFloat(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x + y),
-            (Primitive::Str(x), Primitive::Str(y)) => Primitive::Str(x + &y),
-            (Primitive::Integer(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x as f64 + y),
-            (Primitive::DoublePrecisionFloat(x), Primitive::Integer(y)) => Primitive::DoublePrecisionFloat(x + y as f64),
-            (Primitive::Integer(x), Primitive::Str(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::Str(x), Primitive::Integer(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => Primitive::Str(format!("{}{}", x, y)),
-            (Primitive::Str(x), Primitive::DoublePrecisionFloat(y)) => Primitive::Str(format!("{}{}", x, y)),
+            (Primitive::Integer(x), Primitive::Integer(y)) => Primitive::Integer(x / y),
+            (Primitive::DoublePrecisionFloat(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x / y),
+            (Primitive::Str(a), Primitive::Str(b)) => {
+                let mut result = Vec::new();
+                let words: Vec<&str> = a.split(&b).collect();
+                for s in words {
+                    result.push(Primitive::Str(s.to_string()));
+                }
+                Primitive::Vector(result)
+            },
+            (Primitive::Vector(a), Primitive::Vector(b)) => {
+                let mut result = Vec::new();
+                let mut longer = a.clone();
+                let mut shorter = b.clone();
+                if a.len() < b.len() {
+                    longer = b.clone();
+                    shorter = a.clone();
+                }
+                for (x, y) in shorter.iter().zip(longer.iter()) {
+                    result.push(x.clone() / y.clone());
+                }
+                result.extend(longer[shorter.len()..].iter().cloned());
+                Primitive::Vector(result)
+            },
+            (Primitive::Integer(x), Primitive::DoublePrecisionFloat(y)) => Primitive::DoublePrecisionFloat(x as f64 / y),
+            (Primitive::DoublePrecisionFloat(x), Primitive::Integer(y)) => Primitive::DoublePrecisionFloat(x / y as f64),
+            (Primitive::Integer(x), Primitive::Str(y)) => {
+                let sub_len = y.len() / x as usize;
+                let mut result = String::new();
+                for (i, c) in y.chars().enumerate() {
+                    if i <= sub_len {
+                        result.push(c);
+                    }
+                }
+                Primitive::Str(result)
+            },
+            (Primitive::Str(y), Primitive::Integer(x)) => {
+                let sub_len = y.len() / x as usize;
+                let mut result = String::new();
+                for (i, c) in y.chars().enumerate() {
+                    if i <= sub_len {
+                        result.push(c);
+                    }
+                }
+                Primitive::Str(result)
+            },
+            (Primitive::DoublePrecisionFloat(x), Primitive::Str(y)) => {
+                let l = y.len() as i32;
+                let sub_len = l as f64 / x;
+                let size = sub_len.ceil() as i32 as usize;
+                let mut result = String::new();
+                for (i, c) in y.chars().enumerate() {
+                    if i <= size {
+                        result.push(c);
+                    }
+                }
+                Primitive::Str(result)
+            },
+            (Primitive::Str(y), Primitive::DoublePrecisionFloat(x)) => {
+                let l = y.len() as i32;
+                let sub_len = l as f64 / x;
+                let size = sub_len.ceil() as i32 as usize;
+                let mut result = String::new();
+                for (i, c) in y.chars().enumerate() {
+                    if i <= size {
+                        result.push(c);
+                    }
+                }
+                Primitive::Str(result)
+            },
+            (Primitive::Integer(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::Integer(value)).collect()),
+            (Primitive::DoublePrecisionFloat(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Str(value), Primitive::Vector(y)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::Str(value.clone())).collect()),
+            (Primitive::Vector(y), Primitive::Integer(value)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::Integer(value)).collect()),
+            (Primitive::Vector(y), Primitive::DoublePrecisionFloat(value)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::DoublePrecisionFloat(value)).collect()),
+            (Primitive::Vector(y), Primitive::Str(value)) => Primitive::Vector(y.iter().map(|x| x.clone() / Primitive::Str(value.clone())).collect()),
     }
     }
 }
